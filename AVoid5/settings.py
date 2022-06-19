80f6278313278aaef7c52e060f99f23c478ece71
whoibrar <whoibrar@outlook.com>
2022-06-19 06:22:08 +0530

    update requirements

--- AVoid5/settings.py
+++ AVoid5/settings.py
@@ -145,3 +145,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+# heroku
+
+django_heroku.settings(locals())
